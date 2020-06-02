057dee8cb6f78d42dc076fe0cf76ae29db1f718c
ruhitbaidya <ruhitbaidya01@gmail.com>
2020-06-02 19:46:40 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+var mixer = mixitup('.grid-protfolio-container');
+});
+
+        $(function() {
+          $.scrollify({
+            section : ".smoothScroll",
+              setHeights: false,
+              updateHash: false,
+          });
+        });
+
+
